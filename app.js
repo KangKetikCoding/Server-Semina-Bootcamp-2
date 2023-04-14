77fe86793c8e5ebf37237ea858bc9f5f88eea59a
KangKetikCoding <virtuallinux@DESKTOP-KMM9F96.localdomain>
2023-04-14 20:20:21 +0700

    Integrated Router with app.js

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const logger = require('morgan');
 
 const app = express();
 
+const categoriesRouter = require('./app/api/v1/categories/router');
+
+const v1 = '/api/v1/cms';
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
-    message: 'Wellcome to API Semina'
+    message: 'Wellcome to API Semina',
   });
 });
+
+app.use(v1, categoriesRouter);
 
 module.exports = app;
