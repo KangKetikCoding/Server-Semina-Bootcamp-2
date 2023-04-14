9c45ea0960921f9f22fbdcb88fb7dec8bd93894b
KangKetikCoding <virtuallinux@DESKTOP-KMM9F96.localdomain>
2023-04-14 19:57:38 +0700

    Setup a Directory

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 const app = express();
 
 app.use(logger('dev'));
@@ -14,7 +11,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', (req, res) => {
+  res.status(200).json({
+    message: 'Wellcome to API Semina'
+  });
+});
 
 module.exports = app;
